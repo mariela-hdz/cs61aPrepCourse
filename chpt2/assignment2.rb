00ff8847eb739abf644b2077e02500c7c2a97b3a
Mariela Hernandez <mariela.hdz811@gmail.com>
2016-12-21 20:53:53 -0800

    finished chp2

---
+++ chpt2/assignment2.rb
@@ -0,0 +1,16 @@
+#hours in a year
+
+#if leap year
+puts 24*366
+
+#if not
+puts 24*365
+    
+#minutes in a decade
+puts 60*24*(365*10)
+    
+#age in seconds
+puts (60*60)*(24*365)*18
+
+#author's age
+puts 1160000000/(60*60*24*365)
