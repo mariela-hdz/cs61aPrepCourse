e8dcc10b994a8890ef7fff62f56fcbe0096a2197
Mariela Hernandez <mariela.hdz811@gmail.com>
2016-12-28 14:43:21 -0800

    finished a few things to try in chpt6

---
+++ chpt6/TableOfContents.rb
@@ -0,0 +1,13 @@
+line_width = 60
+str = 'Table of Contents'
+puts(str.center(line_width))
+puts(" ")
+com1 = 'Chapter 1:  Getting Started'
+com2 = 'page  1'
+puts(com1.ljust(line_width/2) + com2.rjust(line_width/2))
+com3 = 'Chapter 2:  Numbers'
+com4 = 'page  9'
+puts(com3.ljust(line_width/2) + com4.rjust(line_width/2))
+com5 = 'Chapter 3:  Letters'
+com6 = 'page 13'
+puts(com5.ljust(line_width/2) + com6.rjust(line_width/2))
