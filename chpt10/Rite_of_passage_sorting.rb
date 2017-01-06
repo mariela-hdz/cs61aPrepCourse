de8aa6206993c34a96ad4340d08c0386fd879a13
Mariela Hernandez <mariela.hdz811@gmail.com>
2017-01-06 00:13:05 -0800

    finished a few things to try in chpt9

---
+++ chpt10/Rite_of_passage_sorting.rb
@@ -0,0 +1,29 @@
+# Rite of Passage Sorting
+
+def sort array 
+	rec_sort array, []
+end
+
+def rec_sort unsorted, sorted
+	if unsorted.length <= 0
+		return sorted 
+	end
+
+smallest = unsorted.pop
+still_unsorted = []
+
+unsorted.each do |tested_object|
+	if tested_object < smallest 
+		still_unsorted.push smallest 
+		smallest = tested_object
+	else
+		still_unsorted.push tested_object
+	end
+end
+
+	sorted.push smallest
+
+	rec_sort still_unsorted, sorted
+end
+
+puts(sort([ 'can', 'feel', 'singing', 'like', 'a', 'can']))
